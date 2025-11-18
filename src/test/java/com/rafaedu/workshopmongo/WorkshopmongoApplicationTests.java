d81bd7ca7c5c42ec7886e1a4715555cd7e04a0f9
EduardoJuvenasso <eduardojuvenasso@gmail.com>
2025-11-18 18:58:38 -0300

    Project created

---
+++ src/test/java/com/rafaedu/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafaedu.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
