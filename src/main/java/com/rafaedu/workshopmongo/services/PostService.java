a9c58f3dcf2e18ec5c98830fa447c9c1501020e3
EduardoJuvenasso <eduardojuvenasso@gmail.com>
2025-11-25 15:45:25 -0300

    Retrieving a post by ID

---
+++ src/main/java/com/rafaedu/workshopmongo/services/PostService.java
@@ -0,0 +1,27 @@
+package com.rafaedu.workshopmongo.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.rafaedu.workshopmongo.domain.Post;
+import com.rafaedu.workshopmongo.domain.User;
+import com.rafaedu.workshopmongo.dto.UserDTO;
+import com.rafaedu.workshopmongo.repository.PostRepository;
+import com.rafaedu.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+	}
+
+
+}
