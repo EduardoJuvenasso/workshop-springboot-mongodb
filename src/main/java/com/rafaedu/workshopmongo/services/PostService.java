1f663f1113cf7bc0ba672b9ad246d1f47f9da58e
EduardoJuvenasso <eduardojuvenasso@gmail.com>
2025-11-27 16:16:54 -0300

    Consultation with multiple criteria

--- src/main/java/com/rafaedu/workshopmongo/services/PostService.java
+++ src/main/java/com/rafaedu/workshopmongo/services/PostService.java
@@ -1,5 +1,6 @@
 package com.rafaedu.workshopmongo.services;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -7,8 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.rafaedu.workshopmongo.domain.Post;
-import com.rafaedu.workshopmongo.domain.User;
-import com.rafaedu.workshopmongo.dto.UserDTO;
 import com.rafaedu.workshopmongo.repository.PostRepository;
 import com.rafaedu.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -26,5 +25,9 @@ public class PostService {
 	public List<Post> findByTitle(String text){
 		return repo.searchTitle(text);
 	}
-
+	
+	public List<Post> fullSearch(String text, Date minDate, Date maxDate) {
+		maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 * 1000);
+		return repo.fullSearch(text, minDate, maxDate);
+	}
 }
